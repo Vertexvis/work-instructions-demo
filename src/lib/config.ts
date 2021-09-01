74610b44700c1f704aeff190d707a808f497af8f
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-09-01 13:00:35 -0600

    Update scene ID and stream key for v2 metadata

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -18,12 +18,9 @@ export const Config: Configuration = {
 // Super Car
 export const Credentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "XSk-ui2P2xd68lkPAEYexgGQqxRdJ3xehhvw",
-  // clientId: "0670FB5312C6491905007BCCD3EF70FB12648EAC24BBDE8B76A1A4F1057B68D7",
-  // streamKey: "nac0DHFMNrjK4gYHnoF5Nb2RuJL88bJHb_7w",
+  streamKey: "cEGp505foa8t6Y-1jIcClhkgstTpNoRYlMsg",
 };
-export const SceneId = "818afd7f-f46c-4b78-b166-2de868eadddf";
-// export const SceneId = "2372b22a-2b38-4178-a4e3-e884e9f0bad9";
+export const SceneId = "421a79ac-9b12-49b1-b517-4d6760b3c9f8";
 
 function envVar(name: string, fallback: string): string {
   const ev = process.env[name];
