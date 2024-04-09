4f969fafefed4569399116b47edb162bb2966f35
Madison Ehlers <49169871+MadisonEhlers-Vertex@users.noreply.github.com>
2024-04-09 11:08:29 -0500

    Update config.ts (#223)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -18,7 +18,7 @@ export const Config: Configuration = {
 // Super Car
 export const Credentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "ocgUAlbpe5dWkOjkHjUWzv7Sm1qWJpTi9sa4",
+  streamKey: "wzTRPxHgI8ylWKevQIDPA-GhL_8PhWwqLj4w",
 };
 export const SceneId = "421a79ac-9b12-49b1-b517-4d6760b3c9f8";
 
