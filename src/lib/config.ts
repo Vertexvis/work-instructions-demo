d2331bdb4a67db8d540b561580a23edaeb33408f
Justin Rodriguez <justin.rodriguez@vertex3d.com>
2024-09-26 16:08:41 -0400

    revert clientId and streamKey to not pull from env
    values are not available at build time, resulting in a failed scene render

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -18,8 +18,11 @@ export const Config: Configuration = {
 };
 
 export const Credentials: StreamCredentials = {
-	clientId: envVar('VERTEX_CLIENT_ID', ''),
-	streamKey: envVar('VERTEX_STREAM_KEY', ''),
+	//clientId: envVar('NEXT_PUBLIC_VERTEX_CLIENT_ID', ''),
+	//streamKey: envVar('NEXT_PUBLIC_VERTEX_STREAM_KEY', ''),
+
+	clientId: '08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA',
+	streamKey: 'wzTRPxHgI8ylWKevQIDPA-GhL_8PhWwqLj4w',
 };
 
 export const SceneId = envVar('VERTEX_SCENE_ID', '');
