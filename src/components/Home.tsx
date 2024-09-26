a43b06bc81a45ebfbd152943baa4adac477d1631
Justin Rodriguez <justin.rodriguez@vertex3d.com>
2024-09-26 16:10:49 -0400

    remove unused code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
 import { BottomDrawer } from '@components/BottomDrawer';
-// import { Header } from '@components/Header';
 import {
 	BottomDrawerHeight,
 	Layout,
@@ -8,19 +7,12 @@ import {
 import { ReportIssueDialog } from '@components/ReportIssueDialog';
 import { Content, RightDrawer } from '@components/RightDrawer';
 import { Viewer } from '@components/Viewer';
-import {
-	//createSceneViewState,
-	//initializeScene,
-	//renderPartRevision,
-	RenderPartRevisionReq,
-} from '@lib/authoring';
 import { Configuration } from '@lib/config';
 import {
 	flyTo,
 	handleHit as onSelect,
 	selectBySuppliedIds,
 } from '@lib/scene-items';
-import { head } from '@lib/utils';
 import { useViewer } from '@lib/viewer';
 import {
 	DefaultInstructions,
@@ -28,63 +20,30 @@ import {
 	WorkInstructions,
 } from '@lib/work-instructions';
 import Snackbar from '@mui/material/Snackbar';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 export function Home({ vertexEnv }: Configuration): JSX.Element {
-	const router = useRouter();
 	const viewer = useViewer();
 
 	const [activeStep, setActiveStep] = React.useState<{
 		num: number;
 		step: InstructionStep | undefined;
 	}>({ num: -1, step: undefined });
-	const [sceneViewId, setSceneViewId] = React.useState<string | undefined>(
-		undefined,
-	);
-	const [ready, setReady] = React.useState(false);
+
+	const [isReportIssueDialogOpen, setIsReportIssueDialogOpen] =
+		React.useState(false);
+	const [ghosted, setGhosted] = React.useState(false);
+	const [isInitialView, setIsInitialView] = React.useState(true);
+	const [isSceneReady, setIsSceneReady] = React.useState(false);
+	// const [selectedPartName, setSelectedPartName] = React.useState<
+	// 	string | undefined
+	// >();
 	const [rightDrawerContent, setRightDrawerContent] = React.useState<
 		Content | undefined
 	>('instructions');
-	const [ghosted, setGhosted] = React.useState(false);
-	const [dialogOpen, setDialogOpen] = React.useState(false);
-	const [snackOpen, setSnackOpen] = React.useState(false);
-	const [selected, setSelected] = React.useState<RenderPartRevisionReq>({});
-	const [partName, setPartName] = React.useState<string | undefined>();
-	const [instructions, setInstructions] =
-		React.useState<WorkInstructions>(DefaultInstructions);
-	const [isInitialView, setIsInitialView] = React.useState(true);
-	React.useEffect(() => {
-		if (!router.isReady) return;
+	const [isSnackbarOpen, setIsSnackbarOpen] = React.useState(false);
 
-		const inst = head(router.query.instructions);
-
-		if (inst == null) return;
-
-		try {
-			const parsed: WorkInstructions = JSON.parse(
-				Buffer.from(inst, 'base64').toString('utf8'),
-			) as WorkInstructions;
-
-			if (
-				parsed.clientId == null ||
-				parsed.streamKey == null ||
-				parsed.steps == null ||
-				Object.keys(parsed.steps).length === 0
-			) {
-				return;
-			}
-
-			setInstructions(parsed);
-		} catch (e: unknown) {
-			console.error('Invalid instructions.', { data: e });
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [router.isReady]);
-
-	React.useEffect(() => {
-		setPartName(selected?.part?.name);
-	}, [selected]);
+	const instructions: WorkInstructions = DefaultInstructions;
 
 	async function handleSceneReady() {
 		const v = viewer.ref.current;
@@ -93,21 +52,15 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 		const scene = await v.scene();
 		if (scene == null) return;
 
-		setReady(true);
-		setSceneViewId(scene.sceneViewId);
-		// if (authoring) await initializeScene({ instructions, viewer: v });
+		setIsSceneReady(true);
 	}
 
 	async function onInstructionStepSelected(num: number): Promise<void> {
-		if (!ready) return;
-		const step = instructions.steps[Object.keys(instructions.steps)[num]];
-		setReady(false);
+		console.log('onInstructionStepSelected');
 
-		function onComplete() {
-			handleInitialView();
-			setActiveStep({ num, step });
-			setReady(true);
-		}
+		if (!isSceneReady) return;
+		const step = instructions.steps[Object.keys(instructions.steps)[num]];
+		setIsSceneReady(false);
 
 		const res = await flyTo({
 			camera: step?.camera,
@@ -115,11 +68,11 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 		});
 
 		if (res) {
-			res.onAnimationCompleted.on(onComplete);
+			res.onAnimationCompleted.on(() => onComplete(num, step));
 			return;
 		}
 
-		onComplete();
+		onComplete(num, step);
 	}
 
 	async function handleBeginAssembly() {
@@ -127,13 +80,19 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 		await onInstructionStepSelected(0);
 	}
 
+	function onComplete(num: number, step: InstructionStep): void {
+		handleInitialView();
+		setActiveStep({ num, step });
+		setIsSceneReady(true);
+	}
+
 	function handleInitialView() {
 		if (isInitialView) {
-			setGhosted(true);
 			setIsInitialView(false);
 		}
 	}
-	return router.isReady ? (
+
+	return (
 		<Layout
 			bottomDrawer={
 				<BottomDrawer
@@ -142,22 +101,10 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 					onSelect={(num: number) => {
 						void onInstructionStepSelected(num);
 					}}
-					ready={ready}
+					ready={isSceneReady}
 				/>
 			}
 			bottomDrawerHeight={BottomDrawerHeight}
-			// header={
-			// 	authoring && (
-			// 		<Header
-			// 			onCreateSceneViewState={(name) => {
-			// 				void createSceneViewState({ name, sceneViewId });
-			// 			}}
-			// 			onRenderPartRevision={() => {
-			// 				void renderPartRevision(selected);
-			// 			}}
-			// 		/>
-			// 	)
-			// }
 			main={
 				viewer.isReady && (
 					<Viewer
@@ -172,33 +119,13 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 							) {
 								setRightDrawerContent(button);
 							} else if (button === 'issue') {
-								setDialogOpen(true);
+								setIsReportIssueDialogOpen(true);
 							}
 						}}
 						onSceneReady={() => {
 							void handleSceneReady();
 						}}
 						onSelect={async (detail, hit) => {
-							console.debug({
-								hitNormal: hit?.hitNormal,
-								hitPoint: hit?.hitPoint,
-								partName:
-									hit?.metadataProperties?.find(
-										(p) => p.key && p.key === 'Name',
-									)?.asString ?? undefined,
-								sceneItemId: hit?.itemId?.hex,
-								sceneItemSuppliedId: hit?.itemSuppliedId?.value,
-							});
-							setSelected({
-								part: {
-									name:
-										hit?.metadataProperties?.find(
-											(p) => p.key && p.key === 'Name',
-										)?.asString ?? undefined,
-									revisionId: hit?.partRevisionId?.hex ?? undefined,
-								},
-								sceneItemSuppliedId: hit?.itemSuppliedId?.value ?? undefined,
-							});
 							await onSelect({ detail, hit, viewer: viewer.ref.current });
 						}}
 						streamKey={instructions.streamKey}
@@ -218,7 +145,7 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 					onClose={() => setRightDrawerContent(undefined)}
 					open={rightDrawerContent != null}
 					onShow={(name, ids) => {
-						setPartName(name);
+						setSelectedPartName(name);
 						void selectBySuppliedIds({ ids, viewer: viewer.ref.current });
 					}}
 					settings={{
@@ -230,29 +157,27 @@ export function Home({ vertexEnv }: Configuration): JSX.Element {
 			}
 			rightDrawerWidth={rightDrawerContent != null ? RightDrawerWidth : 0}
 		>
-			{dialogOpen && (
+			{isReportIssueDialogOpen && (
 				<ReportIssueDialog
-					onClose={() => setDialogOpen(false)}
+					onClose={() => setIsReportIssueDialogOpen(false)}
 					onConfirm={() => {
-						setSnackOpen(true);
-						setDialogOpen(false);
+						setIsSnackbarOpen(true);
+						setIsReportIssueDialogOpen(false);
 					}}
-					open={dialogOpen}
-					partName={partName}
+					open={isReportIssueDialogOpen}
+					partName="none" // TODO {selectedPartName}
 				/>
 			)}
 			<Snackbar
-				open={snackOpen}
+				open={isSnackbarOpen}
 				autoHideDuration={6000}
 				onClose={(_e, reason) => {
 					if (reason === 'clickaway') return;
 
-					setSnackOpen(false);
+					setIsSnackbarOpen(false);
 				}}
 				message="Issue reported successfully."
 			/>
 		</Layout>
-	) : (
-		<></>
 	);
 }
