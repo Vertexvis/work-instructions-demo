2326ce6b2ac67fd4cc07c2d56df2641e17a6c8c0
Justin Rodriguez <justin.rodriguez@vertex3d.com>
2024-09-27 12:44:01 -0400

    remove scene-view-state creation
    Authoring/Scene view creation was not explicity enabled or exposed by the deployed demo. Removing in order to simplify then demo/accelerator code.

--- src/lib/authoring.ts
+++
@@ -1,9 +0,0 @@
-export interface Part {
-	readonly name?: string;
-	readonly revisionId?: string;
-}
-
-export interface RenderPartRevisionReq {
-	readonly part?: Part;
-	readonly sceneItemSuppliedId?: string;
-}
