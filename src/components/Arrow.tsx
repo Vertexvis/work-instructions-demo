d275528665dff2c2dcf9397cd8f9fb48e819a650
Justin Rodriguez <justin.rodriguez@vertex3d.com>
2024-10-04 09:46:36 -0400

    fix formatting

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-/* @jsx jsx */ /** @jsxRuntime classic */ import { jsx } from "@emotion/react";
+/* @jsx jsx */ /** @jsxRuntime classic */ import { jsx } from '@emotion/react';
 
 export function ArrowDown(): JSX.Element {
 	const dimensions = 50;
