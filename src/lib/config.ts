fda177782ffda61b9dbf128bd2b957c8da88b249
Justin Rodriguez <justin.rodriguez@vertex3d.com>
2024-10-04 13:32:26 -0400

    clean up comments

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -18,9 +18,6 @@ export const Config: Configuration = {
 };
 
 export const Credentials: StreamCredentials = {
-	//clientId: envVar('NEXT_PUBLIC_VERTEX_CLIENT_ID', ''),
-	//streamKey: envVar('NEXT_PUBLIC_VERTEX_STREAM_KEY', ''),
-
 	clientId: '08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA',
 	streamKey: 'wzTRPxHgI8ylWKevQIDPA-GhL_8PhWwqLj4w',
 };
